db878f51c2bc84ed0ad69bb472281243b1cf98e8
Emilio Mariscal <emi420@gmail.com>
2014-04-01 17:49:38 -0300

    Bugfix for remote pictures, updated code

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import patterns, include, url
 import settings
 
 urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
     url(r'^', 'app.views.html2pdf'),
+    
 )
 
 
--- app/views.py
+++ app/views.py
@@ -1,15 +1,27 @@
 import requests
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.template.loader import render_to_string
 from django_xhtml2pdf.utils import generate_pdf
 from app import settings
+import StringIO
+import ho.pisa as pisa 
 
 def html2pdf(request):
 
-   url = request.GET['url']
-   r = requests.get(url, verify=False)
-   context = {}
-   resp = HttpResponse(content_type="application/pdf")
-   context['html'] = r.text
-   result = generate_pdf('blank.html', file_object=resp, context=context)
-   return result 
+    url = request.GET['url']
+    r = requests.get(url, verify=False)
+
+    html = render_to_string('blank.html', {'html': r.text},
+                            context_instance=RequestContext(request))
+    result = StringIO.StringIO()
+    pdf = pisa.pisaDocument(
+            StringIO.StringIO(html.encode('ascii', 'xmlcharrefreplace')),
+            result, link_callback=link_callback)
+    return HttpResponse(result.getvalue(), mimetype='application/pdf')
+
+
+def link_callback(uri, rel):
+    if uri.find('http') != -1:
+        return uri
+    return os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
